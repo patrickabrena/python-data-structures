20b246510f16596ed0ffd8ae7781f75d67a034ff
Patrick Abrena <patrickabrena@hotmail.com>
2024-08-21 15:03:14 -0500

    add files to show how to create files within try and except statements

---
+++ creating_files.py
@@ -0,0 +1,8 @@
+
+try:
+    with open("sample/file_create.txt", "w") as file:
+        #writelines accepts a list (so square brackets with "," as a separtor)
+        file.writelines(["\nUsing \nNewline \nCharacters", "\nWritelines accepts a list or an array of strings",    "\nCheck the code out on creating_files.py"])
+except FileNotFoundError as e:
+    print("Can't locate file.", e)
+    #in Line 3, within the with open(), if you put sample/file_create.txt it will throw this error because there is no sample directory
