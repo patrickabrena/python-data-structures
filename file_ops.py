ccac21b90cdd630579f1699df2731f8f7bc5612c
Patrick Abrena <patrickabrena@hotmail.com>
2024-09-18 10:22:47 -0500

    finish file_ops with correct answers and finish module 2 python

--- file_ops.py
+++ file_ops.py
@@ -40,9 +40,7 @@ def read_file_into_list(file_name):
     """
     ### WRITE SOLUTION HERE
     with open(file_name, "r") as file:
-        list = file.read().split("\n")
-        #print(list)
-        #print(list[1]) #testing to see if it prints second line
+        list = file.readlines()
         return list
 
     #raise NotImplementedError()
@@ -63,13 +61,9 @@ def write_first_line_to_file(file_contents, output_filename):
         output_filename: the name of the file to be written to
     """
     ### WRITE SOLUTION HERE
-    # so need to write the first line of file_content and write it to output_filename variable
-    # islolate first line, try print(list[0]) to see if it appears in the console
-    file_contents_list = file_contents.split("\n")
-    the_first_line = file_contents_list[0]
+    split_lines = file_contents.split("\n")
     with open(output_filename, "w") as file:
-        file.write(the_first_line)
-        
+        file.write(split_lines[0])
 
     #raise NotImplementedError()
 
@@ -89,27 +83,12 @@ def read_even_numbered_lines(file_name):
         list: a list of the even-numbered lines of the file
     """
     ### WRITE SOLUTION HERE
-    # use with open() and read file_name into some variable
-    # read the file line by line using .split method into another variable
-    # use a for loop to iterate through the length of that list generated from the split method
-    # use modulo operator to add that element to another list that will be returned at the end
-
-    # init indices_to_remove = []
-    indices_to_remove = []
-
     with open(file_name, "r") as file:
-        contents_in_list = file.read().split("\n")
-
-        #for i in len(contents_in_list): # this isn't right, need to have range() wrapped around the len()
-        for i in range(len(contents_in_list)):
-
-            if (i%2 != 0): # condition to pass the current iteration if i % 2 does not equal 0. (pretty much exclude odd numbers)
-                indices_to_remove.append(i)
-
-        print(indices_to_remove)
-    
-    print(contents_in_list)
-        
+        read_file = file.readlines()
+        even_lines = []
+        for lines in range(len(read_file)):
+            if lines % 2 != 0:
+                even_lines.append(read_file[lines])
         
 
     #raise NotImplementedError()
@@ -130,20 +109,21 @@ def read_file_in_reverse(file_name):
         list: list of the lines of the file in reverse order.
     """
     ### WRITE SOLUTION HERE
-    # need to init an empty array "lines_reversed"
-    # need to use split method to put each line in a list
-    # need for loop to iterate through the original list then pop the element of the array and put it in the lines_reversed array
-    lines_reversed = []
-
-    with open(file_name, "r") as file:
-    #create line variable and apply read() on file and .split() method on that file.read()
-        lines = file.read().split("\n")
-
-    while lines:
-        line = lines.pop()
-        lines_reversed.append(line)
-
-    print(lines_reversed)
+   
+    #initialize empty list
+    li = []
+    #read file into list using for loop
+    with open(file_name, 'r') as file:
+        for x in file:
+            li.append(x)
+    
+    #Use stack-like approach to reverse the list
+    #init a new list(array)
+    reversed_list = []
+    while li: #while there are still eements in li, keep appending the popped elements from li array into 
+        reversed_list.append(li.pop())
+    #print(reversed_list)
+    return reversed_list
 
 
     #raise NotImplementedError()
@@ -162,3 +142,122 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+#Function read_file
+#All test cases passed.
+#Function read_file_into_list
+#Short file test did not pass.
+#Expected (newlines added for legibility):
+#
+#['Because I could not stop for Death,\n', 'He kindly stopped for me;\n', 'The carriage held but just ourselves\n', 'And Immortality.']
+#
+#Got (newlines added for legibility):
+#
+#['Because I could not stop for Death,', 'He kindly stopped for me;', 'The carriage held but just ourselves', 'And Immortality.']
+#
+#
+#Long file test did not pass.
+#Expected (newlines added for legibility):
+#
+#['Let me not to the marriage of true minds\n', 'Admit impediments. Love is not love\n', 'Which alters when it alteration finds,\n', 'Or bends with the remover to remove:\n', 'O, no! it is an ever-fix`ed mark,\n', 'That looks on tempests and is never shaken;\n', "It is the star to every wand'ring bark,\n", "Whose worth's unknown, although his heighth be taken.\n", "Love's not Time's fool, though rosy lips and cheeks\n", "Within his bending sickle's compass come;\n", 'Love alters not with his brief hours and weeks,\n', 'But bears it out even to the edge of doom:\n', 'If this be error and upon me proved,\n', 'I never writ, nor no man ever loved.']
+#
+#Got (newlines added for legibility):
+#
+#['Let me not to the marriage of true minds', 'Admit impediments. Love is not love', 'Which alters when it alteration finds,', 'Or bends with the remover to remove:', 'O, no! it is an ever-fix`ed mark,', 'That looks on tempests and is never shaken;', "It is the star to every wand'ring bark,", "Whose worth's unknown, although his heighth be taken.", "Love's not Time's fool, though rosy lips and cheeks", "Within his bending sickle's compass come;", 'Love alters not with his brief hours and weeks,', 'But bears it out even to the edge of doom:', 'If this be error and upon me proved,', 'I never writ, nor no man ever loved.']
+#
+#
+#Empty file test did not pass.
+#Expected (newlines added for legibility):
+#
+#[]
+#
+#Got (newlines added for legibility):
+#
+#['']
+#
+#
+#Function write_first_line_to_file
+#All test cases passed.
+#Function read_even_numbered_lines
+#Short file test did not pass.
+#Expected (newlines added for legibility):
+#
+#['He kindly stopped for me;\n', 'And Immortality.']
+#
+#Got (newlines added for legibility):
+#
+#None
+#
+#
+#Long file test did not pass.
+#Expected (newlines added for legibility):
+#
+#['Admit impediments. Love is not love\n', 'Or bends with the remover to remove:\n', 'That looks on tempests and is never shaken;\n', "Whose worth's unknown, although his heighth be taken.\n", "Within his bending sickle's compass come;\n", 'But bears it out even to the edge of doom:\n', 'I never writ, nor no man ever loved.']
+#
+#Got (newlines added for legibility):
+#
+#None
+#
+#
+#Single line file test did not pass.
+#Expected (newlines added for legibility):
+#
+#[]
+#
+#Got (newlines added for legibility):
+#
+#None
+#
+#
+#Empty file test did not pass.
+#Expected (newlines added for legibility):
+#
+#[]
+#
+#Got (newlines added for legibility):
+#
+#None
+#
+#
+#Function read_file_in_reverse
+#Short file test did not pass.
+#Expected (newlines added for legibility):
+#
+#['And Immortality.', 'The carriage held but just ourselves\n', 'He kindly stopped for me;\n', 'Because I could not stop for Death,\n']
+#
+#Got (newlines added for legibility):
+#
+#None
+#
+#
+#Long file test did not pass.
+#Expected (newlines added for legibility):
+#
+#['I never writ, nor no man ever loved.', 'If this be error and upon me proved,\n', 'But bears it out even to the edge of doom:\n', 'Love alters not with his brief hours and weeks,\n', "Within his bending sickle's compass come;\n", "Love's not Time's fool, though rosy lips and cheeks\n", "Whose worth's unknown, although his heighth be taken.\n", "It is the star to every wand'ring bark,\n", 'That looks on tempests and is never shaken;\n', 'O, no! it is an ever-fix`ed mark,\n', 'Or bends with the remover to remove:\n', 'Which alters when it alteration finds,\n', 'Admit impediments. Love is not love\n', 'Let me not to the marriage of true minds\n']
+#
+#Got (newlines added for legibility):
+#
+#None
+#
+#
+#Single line file test did not pass.
+#Expected (newlines added for legibility):
+#
+#['Because I could not stop for Death,']
+#
+#Got (newlines added for legibility):
+#
+#None
+#
+#
+#Empty file test did not pass.
+#Expected (newlines added for legibility):
+#
+#[]
+#
+#Got (newlines added for legibility):
+#
+#None
